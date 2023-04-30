c06c51d2831e8fc9180603d55be98562ee1b095d
oceanotrash <estivaericjan@gmail.com>
2023-04-30 19:21:05 +0800

    js comment added

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// Get the button element from the HTML file by ID
 const getAdviceButton = document.getElementById("button");
-const fetchAdvice = async() => {
-    const res = await fetch("https://api.adviceslip.com/advice")
-    const data = await res.json()
 
-    console.log(data)
+// Define a function that fetches advice from an API
+const fetchAdvice = async () => {
+  // Make a GET request to the API and wait for the response
+  const res = await fetch("https://api.adviceslip.com/advice");
 
-    document.getElementById("title").innerHTML = `Advice #${data.slip.id}`
-    document.getElementById("text").innerHTML = `"${data.slip.advice}"`
-}
+  // Extract the JSON data from the response and wait for it
+  const data = await res.json();
 
-window.addEventListener("load", fetchAdvice)
+  // Log the data to the console
+  console.log(data);
 
-getAdviceButton.addEventListener("click", fetchAdvice)
+  // Update the advice displayed on the webpage
+  document.getElementById("title").innerHTML = `Advice #${data.slip.id}`;
+  document.getElementById("text").innerHTML = `"${data.slip.advice}"`;
+};
+
+// Call the fetchAdvice function when the window is loaded
+window.addEventListener("load", fetchAdvice);
+
+// Add an event listener to the button that calls the fetchAdvice function when clicked
+getAdviceButton.addEventListener("click", fetchAdvice);
